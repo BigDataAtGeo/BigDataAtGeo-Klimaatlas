a9e9070acff6312cd78e795f4ecd09a5642064ac
Konstantin Herud <konstantin.herud@gmail.com>
2020-05-10 23:02:49 +0200

    Finished projection map

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,49 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  .loader {
+    border: 0.3em solid #f3f3f3; /* Light grey */
+    border-top: 0.3em solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 1.45em;
+    height: 1.45em;
+    animation: spin 2s linear infinite;
+    z-index: 9999;
+    display: inline-block;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+
+  .center-loader {
+    position: absolute;
+    top: calc(50% - 100px);
+    left: calc(50% - 100px);
+    width: 200px;
+    height: 200px;
+    z-index: 9999;
+  }
+
+  .loader .center-loader {
+    width: 200px;
+    height: 200px;
+  }
+
+  .center-loader-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.5;
+    z-index: 99;
+    background-color: black;
+  }
+
+  .blurry {
+    filter: blur(3px);
+  }
 </style>
--- src/components/Widgets/ProjectionMap.vue
+++ src/components/Widgets/ProjectionMap.vue
@@ -1,148 +1,153 @@
 <template>
-    <div class="container" id="leaflet"></div>
+    <l-map id="leaflet" :options="mapOptions" v-bind:class="{ blurry: loading }">
+        <l-tile-layer :url="url" :attribution="attribution"/>
+        <l-rectangle v-for="feature in this.geojson.features"
+                     :bounds="calculateRectangleBounds(feature.geometry.coordinates)"
+                     :options="calculateRectangleStyle(feature.properties)">
+            <l-popup v-if="variable.unit">{{feature.properties.value}} {{variable.unit}}</l-popup>
+            <l-popup v-else>{{feature.properties.value}}</l-popup>
+        </l-rectangle>
+        <l-control v-if="legend" :position="'bottomleft'" class="custom-control-watermark">
+            <div>
+                <span v-if="this.variable.unit">In {{this.variable.unit}}:</span>
+                <div v-for="bar of this.legend.bars">
+                    <i :style="bar.style"/>
+                    <span>{{bar.value}}</span>
+                </div>
+            </div>
+        </l-control>
+    </l-map>
 </template>
 
 <script>
     import {mapState} from "vuex";
-    import L from 'leaflet';
+    import * as L from "leaflet";
+    import {LMap, LTileLayer, LRectangle, LPopup, LControl} from "vue2-leaflet";
     import * as d3 from "d3";
     import axios from 'axios';
 
     export default {
         name: "ProjectionMap",
+        components: {
+            LMap,
+            LTileLayer,
+            LRectangle,
+            LPopup,
+            LControl,
+        },
         computed: {
-            ...mapState(["scenario", "variable", "timerange", "selectionUri"])
+            // set variable separately to directly compute color map
+            ...mapState(["scenario", "variable", "timerange", "selectionUri"]),
         },
         watch: {
             selectionUri: function () {
-                this.initLayers()
+                this.prepareLegend();
+                this.prepareGeoJson();
             },
         },
         data() {
             return {
-                map: null,
-                geojsonLayer: null,
+                geojson: {features: []},
                 legend: null,
-                center: L.latLng(50, 9.97),
-                zoom: 8,
-                maxBounds: L.LatLngBounds(L.latLng(50.687581, 8.755892), L.latLng(49.344371, 11.010188)),
-                rasterSizeInMeters: 1000,
-                baseTileServer: {
-                    url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                legendColorMap: null,
+                loading: false,
+                mapOptions: {
+                    preferCanvas: true,
+                    zoom: 8,
+                    center: [50, 9.97],
+                    maxBounds: [[50.687581, 8.755892], [49.344371, 11.010188]],
+                    rasterSizeInMeters: 1000,
+                    renderer: L.canvas(),
                 },
+                url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }
         },
-        mounted() {
-            this.$nextTick(() => {
-                this.initMap();
-            });
-        },
         methods: {
-            initMap() {
-                this.map = L.map("leaflet", {
-                    preferCanvas: true,
-                    maxBounds: this.maxBounds
-                }).setView(this.center, this.zoom);
+            prepareLegend() {
+                const min = this.variable.min;
+                const max = this.variable.max;
 
-                this.map.eachLayer(function (layer) {
-                    this.map.removeLayer(layer);
-                }.bind(this));
+                const interpolatorName = "interpolate" + this.variable.colormap;
+                let interpolator;
+                if (d3.hasOwnProperty(interpolatorName)) {
+                    interpolator = d3[interpolatorName];
+                } else {
+                    console.error("D3 colormap '" + interpolatorName + "' not found");
+                    interpolator = d3.interpolateViridis;
+                }
+                this.legendColorMap = d3.scaleSequential().domain([min, max]).interpolator(interpolator);
 
-                L.tileLayer(this.baseTileServer.url, {
-                    attribution: this.baseTileServer.attribution
-                }).addTo(this.map);
-
-                this.map.attributionControl.setPrefix('');
+                this.legend = {bars: []};
+                const stepWidth = (Math.ceil(max) - Math.floor(min)) / 10;
+                for (let i = 0; i <= 10; i++) {
+                    const value = Math.floor(min) + i * stepWidth;
+                    this.legend.bars.push({
+                        value: Math.round(value * 10) / 10, // round to one decimal
+                        style: {
+                            backgroundColor: this.legendColorMap(value)
+                        },
+                    });
+                }
             },
-            initLayers() {
+            prepareGeoJson() {
+                this.loading = true;
                 axios.get(`${process.env.VUE_APP_BDATA_API}/all_locations/${this.selectionUri}`)
                     .catch(function (error) {
                         console.error('fetch data error: failed to load JSON from server', error)
                     }).then(function (response) {
-                        const data = response.data;
-
-                        const min = this.variable.min;
-                        const max = this.variable.max;
-
-                        const interpolatorName = "interpolate" + this.variable.colormap;
-                        let interpolator;
-                        if (d3.hasOwnProperty(interpolatorName)) {
-                            interpolator = d3[interpolatorName];
-                        } else {
-                            console.error("D3 colormap '" + interpolatorName + "' not found");
-                            interpolator = d3.interpolateViridis;
-                        }
-
-                        const cmap = d3.scaleSequential().domain([min, max]).interpolator(interpolator);
-
-                        // Remove old geojsonLayer if there is one
-                        if (this.geojsonLayer) {
-                            this.map.removeLayer(this.geojsonLayer);
-                        }
-
-                        this.geojsonLayer = L.geoJSON(data, {
-                            pointToLayer: function (feature, latlng) {
-                                // rect.on('click', loadTimelineData.bind(this));
-                                return L.rectangle(latlng.toBounds(this.rasterSizeInMeters), {
-                                    color: cmap(feature.properties.value),
-                                    weight: 1,
-                                    fillOpacity: 0.5
-                                });
-                            }.bind(this)
-                        }).bindPopup(function (layer) {
-                            let unitSuffix = this.variable.unit ? ' ' + this.variable.unit : '';
-                            return String(layer.feature.properties.value) + unitSuffix;
-                        }.bind(this)).addTo(this.map);
-
-                        // remove old legend
-                        if (this.legend) {
-                            this.map.removeControl(this.legend);
-                        }
-
-                        const legendBarValues = [];
-                        const stepWidth = (Math.ceil(max) - Math.floor(min)) / 10;
-                        for (let i = 0; i <= 10; i++) {
-                            const value = Math.floor(min) + i * stepWidth;
-                            legendBarValues.push(Math.round(value * 10) / 10); // round to one decimal
-                        }
-
-                        // https://leafletjs.com/examples/choropleth/example.html
-                        this.legend = L.control({position: 'bottomright'});
-                        this.legend.onAdd = function (map) {
-                            let div = L.DomUtil.create('div', 'info legend');
-                            let labels = [];
-                            if (this.variable.unit) {
-                                labels.push("In " + this.variable.unit + ":");
-                            }
-
-                            for (let i = legendBarValues.length - 1; i >= 0; i--) {
-                                const legendBar = legendBarValues[i];
-                                labels.push("<div><i style='background-color:" + cmap(legendBar) + "'></i><span>" + legendBar + "</span></div>");
-                            }
-
-                            div.innerHTML = labels.join('');
-                            return div;
-                        }.bind(this);
-                        this.legend.addTo(this.map);
-
-
-                        // Remove loader
-                        // this.mapLoader.remove();
-                        // this.leafletContainer.children().removeClass('blurry');
-
-                    }.bind(this));
+                    this.geojson = response.data;
+                }.bind(this));
+                this.loading = false;
             },
+            // refer to leaflet implementation
+            calculateRectangleBounds(coordinates) {
+                const latAccuracy = 180 * this.mapOptions.rasterSizeInMeters / 40075017;
+                const lngAccuracy = latAccuracy / Math.cos((Math.PI / 180) * coordinates[1]);
+                return [
+                    [coordinates[1] - latAccuracy, coordinates[0] - lngAccuracy],
+                    [coordinates[1] + latAccuracy, coordinates[0] + lngAccuracy]
+                ];
+            },
+            calculateRectangleStyle(properties) {
+                return {
+                    color: this.legendColorMap(properties.value),
+                    weight: 1,
+                    opacity: 0.5,
+                }
+            },
+            rectangleOnClick(cell) {
+                this.$store.commit("addCell", cell);
+            }
         }
     }
 </script>
 
 <style scoped>
-    div.container {
-        background-color: lightblue;
-        width: 100%;
+    .leaflet-control {
+        padding: 6px 8px;
+        font: 14px/16px Arial, Helvetica, sans-serif;
+        background: white;
+        background: rgba(255, 255, 255, 0.8);
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+        border-radius: 5px;
         height: 100%;
-        max-width: none;
-        padding: 0;
+
+        text-align: left;
+        line-height: 18px;
+        color: #555;
+    }
+
+    .leaflet-control i {
+        width: 18px;
+        height: 18px;
+        float: left;
+        margin-right: 8px;
+        opacity: 0.7;
+    }
+
+    .leaflet-control span {
+        float: right;
+        margin-right: 8px;
     }
 </style>
