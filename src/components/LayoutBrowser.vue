595a3485b7529f646d7da53b5cda1964cefcbd09
Konstantin Herud <konstantin.herud@gmail.com>
2020-08-30 10:33:18 +0200

    Fixed scroll map issue, added focus on hover to widgets

--- src/components/LayoutBrowser.vue
+++ src/components/LayoutBrowser.vue
@@ -1,96 +1,117 @@
 <template>
-<div id="layout">
+  <div id="layout">
     <div class="fixed-top" id="selection-container">
-        <SettingsSelection/>
+      <SettingsSelection/>
     </div>
 
     <div class="fixed-top" id="projectionmap-container">
-        <ProjectionMap/>
+      <ProjectionMap/>
     </div>
     <div class="float-right" id="widgets-container">
-        <WidgetShell widgetName="Graph (Live)" class="row justify-content-end rounded p-0" >
-            <LiveLineCarousel/>
-        </WidgetShell>
-<!--        <WidgetShell widgetName="WetterGraph" class="row justify-content-end rounded p-0">-->
-<!--            <Linegraph/>-->
-<!--        </WidgetShell>-->
-        <WidgetShell v-if="this.$store.state.selectedCells.length!=0" widgetName="Wetter (Live)" class="row justify-content-end rounded p-0">
-           <WeatherCarousel/>
-        </WidgetShell>
-        <WidgetShell widgetName="Variable" class="row justify-content-end rounded p-0">
-            <VariableInfo :variable="this.$store.state.variable"></VariableInfo>
-        </WidgetShell>
+      <WidgetShell widgetName="Graph (Live)" class="row justify-content-end rounded p-0 focus-on-hover">
+        <LiveLineCarousel/>
+      </WidgetShell>
+      <!--        <WidgetShell widgetName="WetterGraph" class="row justify-content-end rounded p-0">-->
+      <!--            <Linegraph/>-->
+      <!--        </WidgetShell>-->
+      <WidgetShell v-if="selectedCells.length!==0" widgetName="Wetter (Live)"
+                   class="row justify-content-end rounded p-0 focus-on-hover">
+        <WeatherCarousel/>
+      </WidgetShell>
+      <WidgetShell widgetName="Variable" class="row justify-content-end rounded p-0 focus-on-hover">
+        <VariableInfo :variable="variable"></VariableInfo>
+      </WidgetShell>
     </div>
 
-    <div class="fixed-bottom" id="timeline-container">
-        <Linegraph/>
+    <div class="fixed-bottom focus-on-hover" id="timeline-container">
+      <Linegraph/>
     </div>
 
-</div>
+  </div>
 </template>
 
 <script>
-    import SettingsSelection from "./Widgets/SettingsSelection";
-    import Linegraph from "./Widgets/Linegraph";
-    import ProjectionMap from "./Widgets/ProjectionMap";
-    import WidgetShell from "./Widgets/WidgetShell";
-    import WeatherCarousel from "./Widgets/WeatherCarousel";
-    import VariableInfo from "./Widgets/VariableInfo";
-    import LiveLineCarousel from "@/components/Widgets/LiveLineCarousel";
+import SettingsSelection from "./Widgets/SettingsSelection";
+import Linegraph from "./Widgets/Linegraph";
+import ProjectionMap from "./Widgets/ProjectionMap";
+import WidgetShell from "./Widgets/WidgetShell";
+import WeatherCarousel from "./Widgets/WeatherCarousel";
+import VariableInfo from "./Widgets/VariableInfo";
+import LiveLineCarousel from "@/components/Widgets/LiveLineCarousel";
+import {mapState} from "vuex";
 
-    export default {
-        name: "LayoutBrowser",
-        components: {SettingsSelection, Linegraph, ProjectionMap, WidgetShell, WeatherCarousel,LiveLineCarousel, VariableInfo}
-    }
+export default {
+  name: "LayoutBrowser",
+  components: {
+    SettingsSelection,
+    Linegraph,
+    ProjectionMap,
+    WidgetShell,
+    WeatherCarousel,
+    LiveLineCarousel,
+    VariableInfo
+  },
+  computed: {
+    ...mapState(["selectedCells", "variable"])
+  },
+}
 </script>
 
 <style>
 body {
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
 </style>
 
 <style scoped>
-    #layout {
-        width: 100vw;
-        height: 100vh;
-        max-width: none;
-    }
+#layout {
+  width: 100vw;
+  height: 100vh;
+  max-width: none;
+}
 
-    #selection-container {
-        z-index: 2;
-        margin-top: 5px;
-        margin-left: auto;
-        margin-right: auto;
-        width: 80vw;
-        height: auto;
-    }
-    #timeline-container {
-        margin-left: auto;
-        margin-right: auto;
-        margin-bottom: 10px;
-        height: 35vh;
-        width: 80vw;
-    }
-    #projectionmap-container {
-        z-index: 1;
-        width: 100vw;
-        height: 100vh;
-    }
-    #widgets-container {
-        height: 80vh;
-        display:flex;
-        flex-direction: column;
-        justify-self: end;
-        width: 25vw;
-        margin-right: 1vh;
-        margin-top: 10vh;
-    }
-    #widgets-container .row {
-        z-index: 2;
-        margin-bottom: 1vh;
-        justify-content: right;
-    }
+#selection-container {
+  z-index: 2;
+  margin-top: 5px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80vw;
+  height: auto;
+}
+
+#timeline-container {
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+  max-height: 35vh;
+  max-width: 80vw;
+}
+
+#projectionmap-container {
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+}
+
+#widgets-container {
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-self: end;
+  width: 25vw;
+  margin-right: 1vh;
+  margin-top: 10vh;
+}
+
+#widgets-container .row {
+  z-index: 2;
+  margin-bottom: 1vh;
+  justify-content: right;
+}
+
+.focus-on-hover:hover {
+  z-index: 2000 !important;
+}
 </style>
--- src/components/Widgets/Linegraph.vue
+++ src/components/Widgets/Linegraph.vue
@@ -1,253 +1,251 @@
 <template>
-    <div class="container d-flex justify-content-center">
-        <div class="align-self-center" v-if="isLoading&&!noCell">
-            <div class="spinner-border text-primary loader" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <line-chart id="line-chart"
-                    v-if="!isLoading && chartData"
-                    :chartData="chartData"
-                    :options="chartOptions"/>
+  <div class="container d-flex justify-content-center">
+    <div class="align-self-center" v-if="isLoading&&!noCell">
+      <div class="spinner-border text-primary loader" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
+    <line-chart id="line-chart"
+                v-if="!isLoading && chartData"
+                :chartData="chartData"
+                :options="chartOptions"/>
+  </div>
 </template>
 
 <script>
-    import {mapState} from "vuex";
-    import axios from 'axios';
-    import LineChart from "./LineChart";
-    import * as d3 from "d3";
-    import { colorGenerate } from '../mixins/colorGenerate';
-    export default {
-        name: "Linegraph",
-        mixins:[colorGenerate],
-        components: {LineChart},
-        data() {
-            return {
-                chartData: null,
-                chartOptions: null,
-                loading: false,
-                datasets:[],
-                labels:[],
-                selectedCellsOld:[],
-                noCell:true,
-                oldUri:"",
-            };
-        },
-        computed: {
-            ...mapState(["scenario", "variable", "timerange", "selectedCells", "selectionUri"]),
-            isLoading: {
-                get() {
-                    return this.loading;
-                },
-                set(value) {
-                    this.loading = value;
-                }
-            },
-            ids(){
-                return this.$store.state.ids; 
-            },
-            
-        },
-        watch: {
-            selectionUri(val){
-                if(this.uriSplitter(this.oldUri)!==this.uriSplitter(this.selectionUri)){
-                    this.chartData = null;
-                    this.chartOptions = null;
-                    this.datasets=[];
-                    this.labels=[];
-                    this.addCompleteChartdata(this.selectedCells);
-                    this.oldUri=this.selectionUri;
-                } 
-            },
-            selectedCells(val) {
-                console.log("watcher");   
-                this.loadChartdata(val);
-            }
-        },
-        methods: {
-            uriSplitter(value){
-                var uriSplit=value.split('/');
-                if (uriSplit.length<2) return " ";
-                return uriSplit[0]+uriSplit[1];
-            },
-            loadChartdata(val){
-                if(val.length>0){
-                    this.isLoading = true;
-                    var id=val[val.length-1].properties.id;
-                    var added=true;
-                    var index=0;
-                    this.noCell=false;
-                    for (var i=0; i<this.selectedCellsOld.length;i++){
-                        if(this.selectedCells.includes(this.selectedCellsOld[i]));
-                        else{
-                            added=false;
-                            index=i;
-                        }
-                    }
-                    if(added){
-                        //   BDATG_ORIGIN + '/all_times/' + id + '/' + this.scenario + '/' + this.variable)
-                        axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
-                            .catch(function (error) {
-                                console.error('fetch data error: failed to load JSON from server', error)
-                                this.isLoading = false;
-                            }.bind(this)).then(function (response) {
-                                //check if cell didnt get removed while waiting for api response
-                                if(this.$store.state.ids.indexOf(id)!=-1){
-                                var dataset={data: response.data.data.values, fill: false,};
-                                this.datasets.push(dataset);
-                                this.setColors();      
-                                this.labels.push(response.data.data.keys);
-                                this.drawTimeline(response.data.data.keys);
-                                if(this.datasets.length==this.selectedCells.length){
-                                    this.isLoading = false;
-                                }
-                                }
-                        }.bind(this));
-                    }else if(this.selectedCells.length==1){
-                        this.datasets.splice(0,this.datasets.length);
-                        this.datasets.splice(0,this.datasets.length);
-                        
-                        axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
-                            .catch(function (error) {
-                                console.error('fetch data error: failed to load JSON from server', error)
-                                this.isLoading = false;
-                            }.bind(this)).then(function (response) {
-                                //check if cell didnt get removed while waiting for api response
-                                if(this.$store.state.ids.indexOf(id)!=-1){
-                                    var dataset={data: response.data.data.values, fill: false,};
-                                    this.datasets.push(dataset);
-                                    this.setColors();  
-                                    this.labels.push(response.data.data.keys);
-                                    this.drawTimeline(response.data.data.keys);
-                                    if(this.datasets.length==this.selectedCells.length){
-                                    this.isLoading = false;
-                                }
-                                }
-                        }.bind(this));
-                    }
-                    else{
-                        this.datasets.splice(index,1);
-                        this.labels.splice(index,1);
-                        this.setColors(); 
-                        this.drawTimeline(this.labels[0]);
-                        if(this.datasets.length==this.selectedCells.length){
-                            this.isLoading = false;
-                        }
-                    }
-                    this.selectedCellsOld=Array.from(this.selectedCells);
-                }else{
-                    this.datasets=[];
-                    this.selectedCellsOld=[];
-                    this.isLoading = true;
-                    this.noCell=true;
-                }
-            },
-            addCompleteChartdata(allCells){
-                if(allCells.length!=0){
-                    this.isLoading=true;
-                    this.noCell=false;
-                for(var i=0;i<allCells.length;i++){
-                    var id=allCells[i].properties.id;
-                    axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
-                            .catch(function (error) {
-                                console.error('fetch data error: failed to load JSON from server', error)
-                                this.isLoading = false;
-                            }.bind(this)).then(function (response) {
-                                //check if cell didnt get removed while waiting for api response
-                                if(this.$store.state.ids.indexOf(id)!=-1){
-                                var dataset={data: response.data.data.values, fill: false,};
-                                this.datasets.push(dataset);
-                                this.setColors();      
-                                this.labels.push(response.data.data.keys);
-                                this.drawTimeline(response.data.data.keys);
-                                if(this.datasets.length==this.selectedCells.length){
-                                    this.isLoading = false;
-                                }
-                                }
-                        }.bind(this));
-                }
-                this.selectedCellsOld=Array.from(this.selectedCells);
-                } 
-                else{
-                    this.noCell=true;
-                } 
-            },
-            setColors(){
-                for(var i=0;i<this.datasets.length;i++){
-                    this.datasets[i].borderColor=this.generateColor(this.ids[i],0);
-                }
-            },
-            drawTimeline(data) {
-                this.chartData = {
-                    labels: data,
-                    datasets: Array.from(this.datasets),
-                };
-                this.chartOptions = {
-                    title: {
-                        display: true,
-                        fontSize: 20,
-                        text: `${this.variable.var} bei Szenario ${this.scenario} (Koordinaten: ${this.selectedCells[0].latlng.lat.toFixed(2)}/${this.selectedCells[0].latlng.lng.toFixed(2)})`
-                    },
-                    legend: {
-                        display: false,
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        xAxes: [{
-                            gridLines: {
-                                lineWidth: 2,
-                            },
-                            scaleLabel: {
-                                display: true,
-                                fontSize: 20,
-                                labelString: 'Zeitraum'
-                            }, ticks: {
-                                fontSize: 16,
-                            }
-                        }],
-                        yAxes: [{
-                            gridLines: {
-                                lineWidth: 2,
-                            },
-                            scaleLabel: {
-                                display: true,
-                                labelString: this.variable.var,
-                                fontSize: 20,
-                            },
-                            ticks: {
-                                suggestedMin: this.variable.min,
-                                suggestedMax: this.variable.max,
-                                fontSize: 16,
-                            }
-                        }]
-                    },
-                    tooltips: {
-                        callbacks: {
-                            label: function (tooltipItem) {
-                                return tooltipItem.yLabel;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+import {mapState} from "vuex";
+import axios from 'axios';
+import LineChart from "./LineChart";
+import * as d3 from "d3";
+import {colorGenerate} from '../mixins/colorGenerate';
+
+export default {
+  name: "Linegraph",
+  mixins: [colorGenerate],
+  components: {LineChart},
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null,
+      loading: false,
+      datasets: [],
+      labels: [],
+      selectedCellsOld: [],
+      noCell: true,
+      oldUri: "",
+    };
+  },
+  computed: {
+    ...mapState(["scenario", "variable", "timerange", "selectedCells", "selectionUri"]),
+    isLoading: {
+      get() {
+        return this.loading;
+      },
+      set(value) {
+        this.loading = value;
+      }
+    },
+    ids() {
+      return this.$store.state.ids;
+    },
+
+  },
+  watch: {
+    selectionUri(val) {
+      if (this.uriSplitter(this.oldUri) !== this.uriSplitter(this.selectionUri)) {
+        this.chartData = null;
+        this.chartOptions = null;
+        this.datasets = [];
+        this.labels = [];
+        this.addCompleteChartdata(this.selectedCells);
+        this.oldUri = this.selectionUri;
+      }
+    },
+    selectedCells(val) {
+      console.log("watcher");
+      this.loadChartdata(val);
     }
+  },
+  methods: {
+    uriSplitter(value) {
+      var uriSplit = value.split('/');
+      if (uriSplit.length < 2) return " ";
+      return uriSplit[0] + uriSplit[1];
+    },
+    loadChartdata(val) {
+      if (val.length > 0) {
+        this.isLoading = true;
+        var id = val[val.length - 1].properties.id;
+        var added = true;
+        var index = 0;
+        this.noCell = false;
+        for (var i = 0; i < this.selectedCellsOld.length; i++) {
+          if (this.selectedCells.includes(this.selectedCellsOld[i])) ;
+          else {
+            added = false;
+            index = i;
+          }
+        }
+        if (added) {
+          //   BDATG_ORIGIN + '/all_times/' + id + '/' + this.scenario + '/' + this.variable)
+          axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
+              .catch(function (error) {
+                console.error('fetch data error: failed to load JSON from server', error)
+                this.isLoading = false;
+              }.bind(this)).then(function (response) {
+            //check if cell didnt get removed while waiting for api response
+            if (this.$store.state.ids.indexOf(id) != -1) {
+              var dataset = {data: response.data.data.values, fill: false,};
+              this.datasets.push(dataset);
+              this.setColors();
+              this.labels.push(response.data.data.keys);
+              this.drawTimeline(response.data.data.keys);
+              if (this.datasets.length == this.selectedCells.length) {
+                this.isLoading = false;
+              }
+            }
+          }.bind(this));
+        } else if (this.selectedCells.length == 1) {
+          this.datasets.splice(0, this.datasets.length);
+          this.datasets.splice(0, this.datasets.length);
+
+          axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
+              .catch(function (error) {
+                console.error('fetch data error: failed to load JSON from server', error)
+                this.isLoading = false;
+              }.bind(this)).then(function (response) {
+            //check if cell didnt get removed while waiting for api response
+            if (this.$store.state.ids.indexOf(id) != -1) {
+              var dataset = {data: response.data.data.values, fill: false,};
+              this.datasets.push(dataset);
+              this.setColors();
+              this.labels.push(response.data.data.keys);
+              this.drawTimeline(response.data.data.keys);
+              if (this.datasets.length == this.selectedCells.length) {
+                this.isLoading = false;
+              }
+            }
+          }.bind(this));
+        } else {
+          this.datasets.splice(index, 1);
+          this.labels.splice(index, 1);
+          this.setColors();
+          this.drawTimeline(this.labels[0]);
+          if (this.datasets.length == this.selectedCells.length) {
+            this.isLoading = false;
+          }
+        }
+        this.selectedCellsOld = Array.from(this.selectedCells);
+      } else {
+        this.datasets = [];
+        this.selectedCellsOld = [];
+        this.isLoading = true;
+        this.noCell = true;
+      }
+    },
+    addCompleteChartdata(allCells) {
+      if (allCells.length != 0) {
+        this.isLoading = true;
+        this.noCell = false;
+        for (var i = 0; i < allCells.length; i++) {
+          var id = allCells[i].properties.id;
+          axios.get(`${process.env.VUE_APP_BDATA_API}/all_times/${id}/${this.scenario}/${this.variable.var_id}`)
+              .catch(function (error) {
+                console.error('fetch data error: failed to load JSON from server', error)
+                this.isLoading = false;
+              }.bind(this)).then(function (response) {
+            //check if cell didnt get removed while waiting for api response
+            if (this.$store.state.ids.indexOf(id) != -1) {
+              var dataset = {data: response.data.data.values, fill: false,};
+              this.datasets.push(dataset);
+              this.setColors();
+              this.labels.push(response.data.data.keys);
+              this.drawTimeline(response.data.data.keys);
+              if (this.datasets.length == this.selectedCells.length) {
+                this.isLoading = false;
+              }
+            }
+          }.bind(this));
+        }
+        this.selectedCellsOld = Array.from(this.selectedCells);
+      } else {
+        this.noCell = true;
+      }
+    },
+    setColors() {
+      for (var i = 0; i < this.datasets.length; i++) {
+        this.datasets[i].borderColor = this.generateColor(this.ids[i], 0);
+      }
+    },
+    drawTimeline(data) {
+      this.chartData = {
+        labels: data,
+        datasets: Array.from(this.datasets),
+      };
+      this.chartOptions = {
+        title: {
+          display: true,
+          fontSize: 20,
+          text: `${this.variable.var} bei Szenario ${this.scenario} (Koordinaten: ${this.selectedCells[0].latlng.lat.toFixed(2)}/${this.selectedCells[0].latlng.lng.toFixed(2)})`
+        },
+        legend: {
+          display: false,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            gridLines: {
+              lineWidth: 2,
+            },
+            scaleLabel: {
+              display: true,
+              fontSize: 20,
+              labelString: 'Zeitraum'
+            }, ticks: {
+              fontSize: 16,
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              lineWidth: 2,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: this.variable.var,
+              fontSize: 20,
+            },
+            ticks: {
+              suggestedMin: this.variable.min,
+              suggestedMax: this.variable.max,
+              fontSize: 16,
+            }
+          }]
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return tooltipItem.yLabel;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
-    .container {
-        width: 100%;
-        height: 100%;
-        max-width: 100%;
-        max-height: 100%;
-        padding: 0;
-    }
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+}
 
-    #line-chart {
-        width: 100%;
-        height: 100%;
-        background-color: rgba(255, 255, 255, 0.75);
-    }
+#line-chart {
+  width: 100%;
+  height: 100%;
+  max-height: 35vh;
+  background-color: rgba(255, 255, 255, 0.75);
+}
 </style>
